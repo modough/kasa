68ff6691ebbdfa310774a2706d539c2a1c8d5f06
mouhamed <mouhamed.mbaye1@outlook.com>
2023-03-29 18:38:14 +0200

    creating collapse components again

--- src/components/collapse.jsx
+++ src/components/collapse.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react';
 import '../styles/collapse.css';
 
@@ -7,33 +7,52 @@ function Collapse() {
     const [isOpen, setOpen] = useState(false);
     const toggleMenu = () => setOpen(!isOpen);
     return (
-        <div className='collapse-container'>
-            <div className='collapse-unit'>
-                <div className='collapse-unit-header'>
+        <ul className='collapse-container'>
+            <li className='collapse-unit'>
+                <span className='collapse-unit-header'>
                     <p>Fiabilité</p>
-                    <FontAwesomeIcon style={{ color: 'white' }} icon={faChevronDown} />
-                </div>
-            </div>
-            <div className='collapse-unit'>
-                <div className='collapse-unit-header'>
+                    <FontAwesomeIcon key={1} onClick={toggleMenu} style={{ color: 'white' }} icon={faChevronDown} />
+                </span>
+                {isOpen && (
+                    <span key={1}>
+                        Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.
+                    </span>
+                )}
+            </li>
+            <li className='collapse-unit'>
+                <span className='collapse-unit-header'>
                     <p>Respect</p>
-                    <FontAwesomeIcon style={{ color: 'white' }} icon={faChevronDown} />
-                </div>
-            </div>
-            <div className='collapse-unit'>
-                <div className='collapse-unit-header'>
+                    <FontAwesomeIcon onClick={toggleMenu} style={{ color: 'white' }} icon={faChevronDown} />
+                </span>
+                {isOpen && (
+                    <span>
+                        La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.
+                    </span>
+                )}
+            </li>
+            <li className='collapse-unit'>
+                <span className='collapse-unit-header'>
                     <p>Service</p>
-                    <FontAwesomeIcon style={{ color: 'white' }} icon={faChevronDown} />
-                </div>
-            </div>
-            <div className='collapse-unit'>
-                <div className='collapse-unit-header'>
+                    <FontAwesomeIcon onClick={toggleMenu} style={{ color: 'white' }} icon={faChevronDown} />
+                </span>
+                {isOpen && (
+                    <span>
+                        Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.
+                    </span>
+                )}
+            </li>
+            <li className='collapse-unit'>
+                <span className='collapse-unit-header'>
                     <p>Responsabilité</p>
-                    <FontAwesomeIcon style={{ color: 'white' }} icon={faChevronDown} />
-                </div>
-            </div>
-
-        </div>
+                    <FontAwesomeIcon onClick={toggleMenu} style={{ color: 'white' }} icon={faChevronDown} />
+                </span>
+                {isOpen && (
+                    <span>
+                        La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.
+                    </span>
+                )}
+            </li>
+        </ul>
     )
 }
 
