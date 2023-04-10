aeed6234f2e137a78da42c9dea211797a60a8868
mouhamed <mouhamed.mbaye1@outlook.com>
2023-04-10 03:13:42 +0200

    troubles wth routes solved

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -22,7 +22,7 @@ export const Router = createBrowserRouter([
         errorElement: <ErrorPage />
     },
     {
-        path: "/housings/:*",
+        path: "/housings/:/*",
         element: <ErrorPage />
     }
 ]);
