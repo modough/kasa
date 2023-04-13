03abbfdff3b75dfdfe88cc20517449f8d17310ef
mouhamed <mouhamed.mbaye1@outlook.com>
2023-04-14 00:15:15 +0200

    refacto router component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom';
 import '../src/styles/index.css';
-import Router from './routes/Router';
+import Router from './routes/RoutingComponent'
 
 
 
